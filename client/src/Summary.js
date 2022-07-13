ae779e68d4a0ed4b397c61ac92537d234ca33a0e
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-13 15:33:50 -0400

    fixed profile page duplicate problem

--- client/src/Summary.js
+++ client/src/Summary.js
@@ -1,23 +1,20 @@
 import React from 'react'
 
-function Summary({expenses, user}) {
+function Summary({expenses, setExpenses, user}) {
+    
+    let tags = []
+    for (const tag in user.tags) {
+        if(!tags.includes(user.tags[tag].name)) {
+            tags.push(user.tags[tag].name)
+        }
+    }       
+
+    debugger 
+
     return(
         <div>
-            <strong>Purchases</strong>
-            {user.tags.map((tag) => {
-                return(
-                    <>
-                        <p>{tag.name}</p>
-                        <ul>
-                        {tag.expenses.map((expense) => {
-                            return(<li key={expense.id}>{expense.description}</li>)
-                            })}
-                        </ul> 
-                    </>
-                )
-                }
-            )}
-
+            <strong>Purchase Categories</strong>
+            {tags.map((tag) => <p key={tag}>{tag}</p>)}
         </div>
     )
 }
